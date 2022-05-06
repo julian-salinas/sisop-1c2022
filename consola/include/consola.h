c1b266072bb7cfa3290470f5b41d64a92b78ab58
pilipiak <aureliapilipiak@gmail.com>
2022-05-06 20:37:50 -0300

    Agrego parseo de instruccion en consola.

--- consola/include/consola.h
+++ consola/include/consola.h
@@ -15,6 +15,7 @@
     #include<commons/log.h>
     #include<commons/string.h>
     #include<commons/config.h>
+    #include<commons/txt.h>
     #include "utils.h"
 
     // Include de las shared
@@ -26,5 +27,16 @@
     #define CONFIG_PATH "/home/utnso/tp2022/tp-2022-1c-Grupo-Final-Final/consola/consola.config"
 
     void terminar_programa(int, t_log*, t_config*);
+
+    /*
+     * @DESC: Mapea un string contra su enum correspondiente
+     * @param identificador: string a mapear
+     */ 
+    t_identificador mapear_identificador(char* identificador);
+
+    /*
+     * @DESC: Agrega parámetros a una instruccion según identificador
+     */ 
+    void agregar_parametros(t_identificador identificador, t_instruccion** instruccion, t_list* parametros);
 
 #endif /* CONSOLA_H */
--- consola/src/consola.c
+++ consola/src/consola.c
@@ -1,7 +1,7 @@
 #include "consola.h"
 
 
-int main(int argc, const char *argv[]) {
+int main(int argc, char** argv) {
 
 	int conexion_kernel;
 	char* ip_kernel;
@@ -10,11 +10,10 @@ int main(int argc, const char *argv[]) {
 	t_log* logger;
 	t_config* config;
 
-	// Crear logger y loggear inicio de consola
 	logger = iniciar_logger("cfg/consola.log", "Consola");
+
 	log_info(logger,"Consola iniciada");
 
-	// Obtener configs
 	config = iniciar_config("cfg/consola.config");
 
 	ip_kernel = config_get_string_value(config,"IP_KERNEL");
@@ -22,30 +21,103 @@ int main(int argc, const char *argv[]) {
 
 	conexion_kernel = crear_socket_cliente(ip_kernel,puerto_kernel);
 
-	/* ----------------------------------------------------------
-	Voy a crear una instrucción de ejemplo para mandarla a kernel 
-	------------------------------------------------------------ */
-	// Armo una lista de instrucciones random
-	t_instruccion* instruccion_ejemplo = crear_instruccion(EXIT);
-	t_lista_instrucciones* lista_instrucciones_ejemplo = crear_lista_instrucciones();
+	if(argc < 2){
+		log_info(logger, "No se recibieron los parámetros correctos. Formato: {cantParametros} {tamañoProceso} {pathArchivo}");
+		terminar_programa(conexion_kernel, logger, config);
+		return 0;
+	}
 
-	agregar_parametro_a_instruccion(instruccion_ejemplo, 4);
-	agregar_parametro_a_instruccion(instruccion_ejemplo, 8);
+	size_t size_proceso = atoi(argv[0]);
+	char* file_path = argv[1];
+	ssize_t read; //cantidad de caracteres leídos
+	char* line = NULL; //línea leída
+	size_t len = 0; //tamaño de la línea leída
+	
+	t_lista_instrucciones* lista_instrucciones = crear_lista_instrucciones();
+	FILE* file_instrucciones = fopen(file_path, "r");
+	if (file_instrucciones == NULL){
+		log_info(logger, "No se pudo abrir el archivo en el path: %s", file_path);
+		terminar_programa(conexion_kernel, logger, config);
+		return 0;
+	}
 
-	agregar_instruccion_a_lista(lista_instrucciones_ejemplo, instruccion_ejemplo);
+	while((read = getline(&line, &len, file_instrucciones)) != -1){
+		//Por cada línea que leo, obtengo los tokens, armo la instrucción con sus parámetros y la agrego a la lista
+		t_list* lines = list_create();
+		char* t = strtok(line, "\n");
+		char* token = strtok(t, " ");
+		list_add(lines, token);
+		while(token != NULL){
+			list_add(lines, token);
+			token = strtok(NULL, " ");
+		}
 
- 	enviar_lista_instrucciones(conexion_kernel, lista_instrucciones_ejemplo);
+		t_identificador identificador = mapear_identificador(list_get(lines, 0));
+		if(identificador == NULL) { 
+			log_info(logger, "Identificador inválido.");
+			terminar_programa(conexion_kernel, logger, config);
+			return 0;
+		}
+		t_instruccion* instruccion = crear_instruccion(identificador);
+		agregar_parametros(identificador, &instruccion, lines);
+		agregar_instruccion_a_lista(lista_instrucciones, instruccion);
+	}
 
-	destruir_lista_instrucciones(lista_instrucciones_ejemplo);
-	/*------------------------------------------------------------ */
+	enviar_lista_instrucciones(conexion_kernel, lista_instrucciones);
+
+	destruir_lista_instrucciones(lista_instrucciones);
 
 	terminar_programa(conexion_kernel, logger, config);
 }
 
-
+//TO DO: Pasar esto a shared
 void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
+	log_info(logger, "Consola finalizada.");
 	liberar_socket_cliente(conexion);
 	log_destroy(logger);
 	config_destroy(config);
+}
+//
+
+t_identificador mapear_identificador(char* identificador){
+	
+	t_identificador cod_identificador;
+		
+		if(strcmp(identificador, "NO_OP") == 0){
+			cod_identificador = NO_OP;
+		}
+		else if(strcmp(identificador, "I_O") == 0){
+			cod_identificador = I_O;
+		}
+		else if(strcmp(identificador, "READ") == 0){
+			cod_identificador = READ;
+		}
+		else if(strcmp(identificador, "WRITE") == 0){
+			cod_identificador = WRITE;
+		}
+		else if(strcmp(identificador, "COPY") == 0){
+			cod_identificador = COPY;
+		}
+		else if(strcmp(identificador, "EXIT") == 0){
+			cod_identificador = EXIT;
+		}
+		return cod_identificador;
+}
+
+void agregar_parametros(t_identificador identificador, t_instruccion** instruccion, t_list* parametros){
+
+	switch (identificador)
+		{
+		case NO_OP:
+		case READ:
+		case I_O:
+			agregar_parametro_a_instruccion(instruccion, atoi(list_get(parametros, 1)));
+			break;
+		case COPY:
+		case WRITE:
+			agregar_parametro_a_instruccion(instruccion, atoi(list_get(parametros, 1)));
+			agregar_parametro_a_instruccion(instruccion, atoi(list_get(parametros, 2)));
+			break;
+		}
 }
