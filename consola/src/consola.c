4f3056c0c81e3cbd908ec1caf23dd5e1ae9a0cb2
julian-salinas <juliansalinasm@gmail.com>
2022-05-30 19:25:58 -0300

    descomento codigo en consola

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -18,13 +18,13 @@ int main(int argc, char** argv) {
 
 	conexion_kernel = crear_socket_cliente(consola_config -> ip_kernel,consola_config -> puerto_kernel);
 
-	// // Validar que se haya enviado tamanio del proceso y path a archivo de pseudocódigo
-	// if (argc < 3) {
-	// 	log_info(logger, "No se recibieron los parámetros correctos. Formato: {./consola} {cantParametros} {tamañoProceso} {pathArchivo}");
-	// 	terminar_programa("Consola", conexion_kernel, logger);
-	// 	destruir_consola_config(consola_config);
-	// 	return 0;
-	// }
+	// Validar que se haya enviado tamanio del proceso y path a archivo de pseudocódigo
+	if (argc < 3) {
+		log_info(logger, "No se recibieron los parámetros correctos. Formato: {./consola} {cantParametros} {tamañoProceso} {pathArchivo}");
+		terminar_programa("Consola", conexion_kernel, logger);
+		destruir_consola_config(consola_config);
+		return 0;
+	}
 
 	size_t size_proceso = 10; //atoi(argv[1]);
 	char* file_path = "pseudo/enunciado.txt"; // (char*) argv[2];
