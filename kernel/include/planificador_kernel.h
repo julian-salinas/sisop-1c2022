5020cbebbd84ef98aaabf3c369c49d07f6e7a2c9
pilipiak <aureliapilipiak@gmail.com>
2022-05-31 22:50:33 -0300

    Agrego colas de estados, funciones para mover procesos entre colas y mutex de colas.

--- kernel/include/planificador_kernel.h
+++ kernel/include/planificador_kernel.h
@@ -4,6 +4,8 @@
     #include<stdio.h>
     #include<stdlib.h>
     #include<commons/config.h>
+    #include <commons/collections/queue.h>
+    #include <semaphore.h>
 
     #include "networking.h"
     #include "serializacion.h"
@@ -20,8 +22,30 @@
     algoritmo_planificacion algoritmo_elegido;
 
     int calcular_estimacion(t_PCB* pcb);
-
     void elegir_algoritmo(char* algoritmo);
+    void iniciar_planificacion();
+    void inicializar_semaforos_plani();
+    void new_a_ready();
+    void new_a_running();
+    void running_a_ready(t_proceso* procesoAMover);
+    void running_a_blocked(t_proceso* procesoAMover);
+    void blocked_a_ready();
+    void blocked_a_exit();
+    void new_a_exit();
+    void running_a_exit(t_proceso* procesoAMover);
+    void ready_a_exit();
+
+    //Colas
+    t_queue* cola_new;
+    t_queue* cola_ready;
+    t_queue* cola_exit;
+    t_queue* cola_blocked;
+
+    //Semáforos colas
+    sem_t* mutex_cola_new;
+    sem_t* mutex_cola_ready;
+    sem_t* mutex_cola_exit;
+    sem_t* mutex_cola_blocked;
 
 
 #endif /* PLANIFICADOR_KERNEL_H */
--- kernel/src/planificador_kernel.c
+++ kernel/src/planificador_kernel.c
@@ -2,6 +2,7 @@
 #include "kernel.h"
 
 int calcular_estimacion(t_PCB* pcb){
+
     //inicializo config para el alfa
     int estimacion_actual = 0, tiempo_ejecucion = 0;
     float alfa = kernel_config -> alfa;
@@ -15,6 +16,7 @@ int calcular_estimacion(t_PCB* pcb){
     pcb ->  estimacion_rafaga = estimacion_actual;
 
     destruir_kernel_config(kernel_config);
+
 
     return estimacion_actual;
 }
@@ -30,4 +32,132 @@ void elegir_algoritmo(char* algoritmo) {
         algoritmo_elegido = SRT;
         log_info(logger, "Se eligio el algoritmo SRT");
     }
+}
+
+void iniciar_planificacion(){
+
+    cola_new = queue_create();
+    cola_ready = queue_create();
+    cola_exit = queue_create();
+    cola_blocked = queue_create();
+
+    inicializar_semaforos_plani();
+}
+
+void inicializar_semaforos_plani(){
+
+    mutex_cola_new = malloc(sizeof(sem_t));
+    sem_init(mutex_cola_new, 0, 1);
+
+    mutex_cola_ready = malloc(sizeof(sem_t));
+    sem_init(mutex_cola_ready, 0, 1);
+
+    mutex_cola_exit = malloc(sizeof(sem_t));
+    sem_init(mutex_cola_exit, 0, 1);
+
+    mutex_cola_blocked = malloc(sizeof(sem_t));
+    sem_init(mutex_cola_blocked, 0, 1);
+}
+
+/* LAS SIGUIENTES FUNCIONES MUEVEN LOS PROCESOS DE UNA COLA A OTRA. HABRÍA QUE MANEJAR ESTADOS Y OBTENER EL PROCESO Y EL PCB */
+
+void new_a_ready(){
+
+    sem_wait(mutex_cola_new);
+    t_proceso* procesoAMover = queue_pop(cola_new);
+    sem_post(mutex_cola_new);
+
+    sem_wait(mutex_cola_ready);
+    queue_push(cola_ready, procesoAMover);
+    sem_post(mutex_cola_ready);
+
+    //log_info(logger, "El proceso con Id:  pasó de NEW a READY.") //acá no tengo el PCB para obtener el id del proceso.
+}
+
+void new_a_running(){
+
+    sem_wait(mutex_cola_new);
+    queue_pop(cola_new);
+    sem_post(mutex_cola_new);
+
+    //log_info(logger, "El proceso con Id:  pasó de NEW a RUNNING.") //acá no tengo el PCB para obtener el id del proceso.
+}
+
+void running_a_ready(t_proceso* procesoAMover){
+
+    sem_wait(mutex_cola_ready);
+    queue_push(cola_new, procesoAMover);
+    sem_post(mutex_cola_ready);
+
+    //log_info(logger, "El proceso con Id:  pasó de RUNNING a READY.") //acá no tengo el PCB para obtener el id del proceso.
+}
+
+void running_a_blocked(t_proceso* procesoAMover){
+
+    sem_wait(mutex_cola_blocked);
+    queue_push(cola_blocked, procesoAMover);
+    sem_post(mutex_cola_blocked);
+
+    //log_info(logger, "El proceso con Id:  pasó de RUNNING a BLOCKED.") //acá no tengo el PCB para obtener el id del proceso.
+}
+
+void blocked_a_ready(){
+
+    sem_wait(mutex_cola_blocked);
+    t_proceso* procesoAMover = queue_pop(cola_blocked);
+    sem_post(mutex_cola_blocked);
+
+    sem_wait(mutex_cola_ready);
+    queue_push(cola_ready, procesoAMover);
+    sem_post(mutex_cola_ready);
+
+    //log_info(logger, "El proceso con Id:  pasó de BLOCKED a READY.") //acá no tengo el PCB para obtener el id del proceso.
+}
+
+void blocked_a_exit(){
+
+    sem_wait(mutex_cola_blocked);
+    t_proceso* procesoAMover = queue_pop(cola_blocked);
+    sem_post(mutex_cola_blocked);
+
+    sem_wait(mutex_cola_exit);
+    queue_push(cola_exit, procesoAMover);
+    sem_post(mutex_cola_exit);
+
+    //log_info(logger, "El proceso con Id:  pasó de BLOCKED a EXIT.") //acá no tengo el PCB para obtener el id del proceso.
+}
+
+void new_a_exit(){
+
+    sem_wait(mutex_cola_new);
+    t_proceso* procesoAMover = queue_pop(cola_new);
+    sem_post(mutex_cola_new);
+
+    sem_wait(mutex_cola_exit);
+    queue_push(cola_exit, procesoAMover);
+    sem_post(mutex_cola_exit);
+
+    //log_info(logger, "El proceso con Id:  pasó de NEW a EXIT.") //acá no tengo el PCB para obtener el id del proceso.
+}
+
+void running_a_exit(t_proceso* procesoAMover){
+
+    sem_wait(mutex_cola_exit);
+    queue_push(cola_exit, procesoAMover);
+    sem_post(mutex_cola_exit);
+
+    //log_info(logger, "El proceso con Id:  pasó de RUNNING a EXIT.") //acá no tengo el PCB para obtener el id del proceso.
+}
+
+void ready_a_exit(){
+
+    sem_wait(mutex_cola_ready);
+    t_proceso* procesoAMover = queue_pop(cola_ready);
+    sem_post(mutex_cola_ready);
+
+    sem_wait(mutex_cola_exit);
+    queue_push(cola_exit, procesoAMover);
+    sem_post(mutex_cola_exit);
+
+    //log_info(logger, "El proceso con Id:  pasó de READY a EXIT.") //acá no tengo el PCB para obtener el id del proceso.
 }
